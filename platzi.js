a9ec2017f095e5b245bdfc6af05eaeffce06865c
JACustodioD <jacustodiod@hotmail.com>
2021-04-12 14:20:39 -0500

    clase 6

--- platzi.js
+++ platzi.js
@@ -1,11 +1,17 @@
 class PlatziReactive{
+    // Dependencias
+    deps = new Map();
+
     constructor(options){
         this.origen = options.data();
+        const self = this;
 
         // Destino
         this.$data = new Proxy(this.origen, {
+
             get(target, name) {
                 if(Reflect.has(target, name)) {
+                    self.track(target, name);
                     return Reflect.get(target, name);
                 }
                 console.warn("La propiedad ",name," no existe");
@@ -13,9 +19,34 @@ class PlatziReactive{
             },
             set(target, name, value ) {
                 Reflect.set(target, name ,value);
+                self.trigger(name);
             }
         })
     }
+    
+    track(target, name){
+        if(!this.deps.has(name)) {
+            const effect = () => {
+
+                document.querySelectorAll(`*[p-text=${name}]`).forEach(el => {
+                    this.pText(el, target,name);
+                });
+
+                document.querySelectorAll("*[p-bind]").forEach(el => {
+                    const [attr, name] =  el.getAttribute("p-bind").match(/(\w+)/g);
+                    this.pBind(el, this.$data, name, attr);
+                });
+
+            };
+            this.deps.set(name, effect);
+        }
+    }
+
+    trigger(name){
+        const effect = this.deps.get(name);
+        effect();
+    }
+
     mount() {
         document.querySelectorAll("*[p-text]").forEach(el => {
             this.pText(el, this.$data , el.getAttribute("p-text"));
@@ -32,13 +63,23 @@ class PlatziReactive{
         
         });
 
+        document.querySelectorAll("*[p-bind]").forEach(el => {
+            const [attr, name] = el.getAttribute("p-bind").match(/(\w+)/g);
+            this.pBind(el, this.$data, name, attr);
+        });
+
     }
 
-   pText(el, target, name){
+    pText(el, target, name){
        el.innerText = Reflect.get(target, name);
-   }
+    }
+    
     pModel(el, target, name){
         el.value = Reflect.get(target, name);
+    }
+
+    pBind(el, target, name, attr){
+        el.setAttribute(attr, Reflect.get(target, name));
     }
 }
 
